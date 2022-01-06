02bdef4572842dee701b9e13d32049df03e6654a
webdsa <71404981+webdsa@users.noreply.github.com>
2022-01-06 12:54:01 -0300

    Update wp-config.php
    Atualiza as configurações de charset e collate no wp-config

--- app/pt/wp-config.php
+++ app/pt/wp-config.php
@@ -28,8 +28,12 @@ define( 'DB_PASSWORD', $_ENV['WP_DB_PASSWORD']);
 /** MySQL hostname */
 define( 'DB_HOST', $_ENV['WP_DB_HOST']);
 
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8mb4');
+
 /** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+define('DB_COLLATE', 'utf8mb4_unicode_ci');
+
 define('SITE', 'tax');
 
 define( 'AS3CF_SETTINGS', serialize( array(
